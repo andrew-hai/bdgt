578dea04814eaede00424f1289b6aaf14a9e58ab
Andrey Melchenko <andrey.melchenko@gmail.com>
2017-11-30 19:19:26 +0200

    Add file upload admin page

--- app/admin/audios.rb
+++ app/admin/audios.rb
@@ -10,7 +10,7 @@ ActiveAdmin.register Audio do
                 :duration,
                 :file
 
-  config.sort_order = 'title_asc'
+  config.sort_order = 'artist_asc'
 
   index do
     selectable_column
@@ -37,14 +37,10 @@ ActiveAdmin.register Audio do
   filter :duration
 
   collection_action :seed, method: :get do
-    if params[:truncate] == 'true'
-      Audio.destroy_all
-    end
-
     files = Dir.entries("#{Rails.root}/public/media").select { |filename| filename =~ /\.mp3|\.flac/i }
 
     files.each do |file|
-      id_3_tag = ID3Tag.read(File.open("#{Rails.root}/public/media/#{file}", 'rb'))        
+      id_3_tag = ID3Tag.read(File.open("#{Rails.root}/public/media/#{file}", 'rb'))
       TagLib::FileRef.open("#{Rails.root}/public/media/#{file}") do |fileref|
 
         Audio.create(
@@ -53,10 +49,49 @@ ActiveAdmin.register Audio do
           title: combined_value(id_3_tag.title, fileref.tag.title),
           album: combined_value(id_3_tag.album, fileref.tag.album),
           year: combined_value(id_3_tag.year, fileref.tag.year),
-          track_number: combined_value(id_3_tag.track_nr.to_i, fileref.tag.track.to_i),
+          track_number: [id_3_tag.track_nr.try(:to_i), fileref.tag.track.try(:to_i)].detect{|n| n > 0},
           genre: combined_value(id_3_tag.genre, fileref.tag.genre),
           duration: fileref.audio_properties.length
         )
+      end
+    end
+
+    render json: true
+  end
+
+  collection_action :upload_form, method: :get do
+  end
+
+  collection_action :upload, method: :post do
+    files = params[:audio][:files]
+
+    files.each do |file|
+      id_3_tag = ID3Tag.read(File.open(file.tempfile.path, 'rb'))
+      TagLib::FileRef.open(file.tempfile.path) do |fileref|
+
+        begin
+          Audio.create(
+            file: File.open(file.tempfile.path),
+            artist: combined_value(id_3_tag.artist, fileref.tag.artist),
+            title: combined_value(id_3_tag.title, fileref.tag.title),
+            album: combined_value(id_3_tag.album, fileref.tag.album),
+            year: combined_value(id_3_tag.year, fileref.tag.year),
+            track_number: [id_3_tag.track_nr.try(:to_i), fileref.tag.track.try(:to_i)].detect{|n| n > 0},
+            genre: combined_value(id_3_tag.genre, fileref.tag.genre),
+            duration: fileref.audio_properties.length
+          )
+        rescue
+          Audio.create(
+            file: File.open(file.tempfile.path),
+            artist: fileref.tag.artist,
+            title: fileref.tag.title,
+            album: fileref.tag.album,
+            year: fileref.tag.year,
+            track_number: fileref.tag.track.try(:to_i),
+            genre: fileref.tag.genre,
+            duration: fileref.audio_properties.length
+          )
+        end
       end
     end
 
@@ -68,7 +103,7 @@ ActiveAdmin.register Audio do
       if val1.present? && val2.present? && val1 != val2
         "#{val1}, #{val2}"
       else
-        val1.presence || val2
+        (val1.presence || val2).to_s
       end
     end
   end
---
+++ config/initializers/id3tag.rb
@@ -0,0 +1,3 @@
+ID3Tag.configuration do |c|
+  c.string_encode_options = { :invalid => :replace, :undef => :replace }
+end
